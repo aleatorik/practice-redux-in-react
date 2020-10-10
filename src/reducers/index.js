cf5f4d26c185d1410e2406e151661c99146d8108
daseulsong <vates0328@gmail.com>
2020-10-10 17:54:59 +0900

    chore: Add a comment

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -14,7 +14,7 @@ const selectedSongReducer = (selectedSong = null, action) => {
   if (action.type === "SONG_SELECTED") {
     return action.payload;
   }
-
+// 추가 타입이 있을 나중의 경우를 대비해서 if 조건문 
   return selectedSong;
 };
 
